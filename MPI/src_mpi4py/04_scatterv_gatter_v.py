4cccc4d618a4309a579268a3f429560b0aab8aaa
Lucas lopes <lucasomena@handtalk.me>
2022-07-19 10:36:10 -0300

    Update 04_scatterv_gatter_v.py

--- MPI/src_mpi4py/04_scatterv_gatter_v.py
+++ MPI/src_mpi4py/04_scatterv_gatter_v.py
@@ -5,9 +5,9 @@ size = comm.Get_size()
 rank = comm.Get_rank()
 
 if rank == 0:
-    n_points = 100
+    n_points = 110
     data = list(range(n_points))
-    step = int(n_points/size)
+    step = int(n_points/size) + 1
     datap = []
     for i in range(0,len(data),step):
         if i+step<len(data):
